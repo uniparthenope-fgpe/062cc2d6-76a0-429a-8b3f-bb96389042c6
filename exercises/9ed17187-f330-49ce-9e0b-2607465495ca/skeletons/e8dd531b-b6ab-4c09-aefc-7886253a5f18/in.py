a7f1e520ab3199157dfa7a85911a097a50e5f73a
Ciro De Vita <ciro.devita001@gmail.com>
2024-10-08 19:56:47 +0200

    Removed exercises/9ed17187-f330-49ce-9e0b-2607465495ca/skeletons/e8dd531b-b6ab-4c09-aefc-7886253a5f18/in.py

--- exercises/9ed17187-f330-49ce-9e0b-2607465495ca/skeletons/e8dd531b-b6ab-4c09-aefc-7886253a5f18/in.py
+++
@@ -1,9 +0,0 @@
-def factorial(n):
-    if n == 0:
-        return 1
-    else:
-        return n * factorial(n - 1)
-
-number = int(input('Enter a number: '))
-result = factorial(number)
-print('Factorial:', result)

a1b404d8ac2db1a22e36d5172b7ce509bb4d0130
Ciro De Vita <ciro.devita001@gmail.com>
2024-10-08 19:53:53 +0200

    created file exercises/534a8828-667d-4091-bf22-913bfb75c30d/skeletons/312cb077-ce30-40b0-9247-fba2d0c24c2b/in.py

---
+++ exercises/534a8828-667d-4091-bf22-913bfb75c30d/skeletons/312cb077-ce30-40b0-9247-fba2d0c24c2b/in.py
@@ -0,0 +1,17 @@
+def add(a, b):
+    # Fill in the code
+
+def subtract(a, b):
+    # Fill in the code
+
+def multiply(a, b):
+    # Fill in the code
+
+def divide(a, b):
+    # Fill in the code
+
+num1 = float(input('Enter first number: '))
+num2 = float(input('Enter second number: '))
+operation = input('Choose operation (+, -, *, /): ')
+
+# Add your code here to call the appropriate function
